506207aa25b72d46c88e6914bf2a8db620118e48
Maciej Lachowicz <maciej.lachowicz@ngkf.com>
2020-05-27 13:48:44 +0200

    Minor fixes, added readme

--- Sqltable_gen.py
+++ Sqltable_gen.py
@@ -1,75 +1,81 @@
-import pandas as pd
 import random
 
 
 class Sql_table_generator:
 
-    def __init__(self,tablename:str=""):
+    def __init__(self, tablename: str = ""):
 
         self.tablename = tablename
         self.result = ""
 
-    #Possible tables: Name, Surname, Fullname, Email, Company, City, Region
-    #only up to 100 recs :(
+    # Possible tables: Name, Surname, Fullname, Email, Company, City, Region
+    # only up to 100 recs :(
 
+    def create_table(self, tablename: str, list_of_colnames: [str], list_of_types: [str]) -> None:
 
-
-
-    def createTable(self,tablename:str,list_of_colnames:[str],list_of_types:[str]):
-        self.tablename=tablename
+        """
+        Function creates sql command to create new table with provided params.
+        :param tablename: name of table to be created
+        :param list_of_colnames: list of strings, containing names of columns to be created
+        :param list_of_types:  list of strings, containing types of columns to be created
+        :return:
+        """
+        self.tablename = tablename
         tablecode = f"CREATE TABLE {tablename}("
         if len(list_of_colnames) != len(list_of_types):
             raise Exception("List of column names has different length than list of types!")
 
         for i in range(len(list_of_colnames)):
-            tablecode+=f"{list_of_colnames[i]} {list_of_types[i]}, "
+            tablecode += f"{list_of_colnames[i]} {list_of_types[i]}, "
 
-        #deleting last comma
+        # deleting last comma
         tablecode = tablecode[:-2] + " );"
-        return tablecode
-                                                    #list of lists
-    def createInserts(self,values:[[]], amount:int =-1, shuffle=False):
-        inserts=""                                     #list of lists
-        #check if all lists in values has proper length
-        length=len(values[0])
+        self.result += tablecode
+        # list of lists
+
+    def create_inserts(self, values: [[]], amount: int = -1, shuffle=False) -> None:
+        """
+        Function create insert commands.
+        :param values: list of lists with string values to be put into
+        :param amount: number of inserts to be created
+        :param shuffle: controls if data should be shuffled
+        :return:
+        """
+        inserts = ""  # list of lists
+        # check if all lists in values has proper length
+        length = len(values[0])
         for i in values:
-            if len(i)!= length:
+            if len(i) != length:
                 raise Exception("All lists")
 
-        #if amount not set, then going for length
-        if amount ==-1:
-            amount=length
-        if amount>length:
+        # if amount not set, then going for length
+        if amount == -1:
+            amount = length
+        if amount > length:
             raise Exception("Amount is higher than max length of datatable")
 
-
-        #shuffling
+        # shuffling
         if shuffle:
             for d in values:
                 random.shuffle(d)
 
         for i in range(amount):
-            inserts+=f"INSERT INTO {self.tablename}, VALUES( "
+            inserts += f"INSERT INTO {self.tablename}, VALUES( "
             for val in values:
-                inserts+=f"'{val[i]}',"
+                inserts += f"'{val[i]}',"
 
-            #deleting last comma
+            # deleting last comma
             inserts = inserts[:-1]
-            inserts+=" );"
+            inserts += " );"
 
+        self.result += inserts
 
-        return inserts
-    def save(self,path=""):
+    def save(self, path=""):
         newpath = ""
         if path.endswith(".txt"):
-            newpath=path
+            newpath = path
         else:  # path.endswith("/") or path.endswith("\\"):
-            newpath=path+f"SQL_buildup-{self.tablename}.txt"
+            newpath = path + f"SQL_buildup-{self.tablename}.txt"
 
         with open(newpath, "w") as target:
             target.write(self.result)
-
-
-
-
-
